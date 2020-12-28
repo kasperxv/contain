eb0832c89088044ac25f3108ec481009681d6d91
Kasper <76576938+kasperxv@users.noreply.github.com>
2020-12-27 23:58:21 -0700

    light specs

--- contain.go
+++ contain.go
@@ -2,14 +2,21 @@ package main
 
 import ()
 
-func main() {}
+func main() {
+    // pass args for "run" "child" "exec"
+}
 
+// when execution refers to itself
 // func parent()
 
+// when creating a sub PID
 // func child()
 
+// general exec running
 // func run()
 
+// error and conditions checking
 // func check()
 
+// for managing cgroups
 // func control()
